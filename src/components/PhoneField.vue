e220b85c15c524fe886d16c0754f77a4fbc03668
Damián Adams <damian.adams@rewardstyle.com>
2021-08-10 17:55:14 +0100

    Add reactive formatting and validation for phone number

--- src/components/PhoneField.vue
+++ src/components/PhoneField.vue
@@ -4,10 +4,14 @@
       <v-layout row wrap>
         <v-flex xs12 sm6 md3>
           <v-text-field
+            v-model="phone"
             id="phone"
+            name="phone"
             placeholder="Phone Number"
             type="tel"
           ></v-text-field>
+          <p>Phone number: {{ realPhoneNumber }}</p>
+          <p>Is valid phone number? {{ isValidPhoneNumber }}</p>
         </v-flex>
       </v-layout>
     </v-container>
@@ -18,11 +22,22 @@
 import intlTelInput from 'intl-tel-input';
 import utils from 'intl-tel-input/build/js/utils';
 
+// Babel config is pretty basic rn - no new logical operators so a lot of boolean short-circuiting for quick reactive computed props
+
 export default {
   data() {
     return {
+      phone: '',
       iti: null
     };
+  },
+  computed: {
+    realPhoneNumber() {
+      return this.phone && this.iti && this.iti.getNumber();
+    },
+    isValidPhoneNumber() {
+      return this.phone && this.iti && this.iti.isValidNumber();
+    }
   },
   mounted() {
     const input = document.querySelector('#phone');
